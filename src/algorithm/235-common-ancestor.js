2c62ed5ccb682a7fb1e4c2940e0ce8e0cbe7852c
azeryang <517538470@qq.com>
2022-10-10 21:09:05 +0800

    feat(leetcode): 235

--- src/algorithm/235-common-ancestor.js
+++ src/algorithm/235-common-ancestor.js
@@ -13,5 +13,13 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+  if (!root) return null;
 
+  const value = root.val;
+  if (value === p.val || value === q.val) return root;
+  
+  if (value === Math.min(value, p.val, q.val)) return lowestCommonAncestor(root.right, p, q);
+  else if (value === Math.max(value, p.val, q.val)) return lowestCommonAncestor(root.left, p, q);
+
+  return root;
 };
