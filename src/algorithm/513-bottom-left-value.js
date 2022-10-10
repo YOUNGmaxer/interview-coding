28ab27f5f2c46120479c9ef8a1a9b4ccafe6fc6b
azeryang <517538470@qq.com>
2022-10-10 18:47:43 +0800

    feat(leetcode): 513

---
+++ src/algorithm/513-bottom-left-value.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+  const queue = [root];
+  let result = root.val;
+
+  while (queue.length) {
+    const len = queue.length;
+    result = queue[0].val;
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift();
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+
+  return result;
+};
