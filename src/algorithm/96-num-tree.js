93079dc7afc656840bf8353fbee3bb7d5f96cae1
azeryang <517538470@qq.com>
2022-10-12 10:38:54 +0800

    feat(leetcode): 96

---
+++ src/algorithm/96-num-tree.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  const dp = new Array(n+1).fill(0);
+
+  dp[0] = 1;
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      dp[i] += dp[j] * dp[i-1-j];
+    }
+  }
+
+  return dp[n];
+};
+
+console.log(numTrees(3));
