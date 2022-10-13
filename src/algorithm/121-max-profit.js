155dddc25d19c2cd94cc63c7b0adf7e5bb302831
azeryang <517538470@qq.com>
2022-10-13 16:12:43 +0800

    feat(leetcode): max-profit

--- src/algorithm/121-max-profit.js
+++ src/algorithm/121-max-profit.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let maxProfit = 0;
+  let max = 0;
+  let low = Infinity;
 
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] > prices[i - 1]) {
-      maxProfit += prices[i] - prices[i - 1];
-    }
+  for (let i = 0; i < prices.length; i++) {
+    low = Math.min(low, prices[i]);
+    max = Math.max(prices[i] - low, max);
   }
-
-  return maxProfit;
+  
+  return max;
 };
 
 var maxProfitByDp = function(prices) {
@@ -19,14 +19,16 @@ var maxProfitByDp = function(prices) {
   const dp = new Array(len).fill(0).map(() => [0, 0]);
 
   dp[0][0] = -prices[0];
+  dp[0][1] = 0;
 
   for (let i = 1; i < len; i++) {
-    dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
+    // 注意，因为只能买入一次，所以当要买入时，就是 -prices[i]
+    dp[i][0] = Math.max(dp[i-1][0], -prices[i]);
     dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
   }
 
   return dp[len-1][1];
-};
+}
 
 console.log(maxProfit([7,1,5,3,6,4]));
 console.log(maxProfitByDp([7,1,5,3,6,4]));
