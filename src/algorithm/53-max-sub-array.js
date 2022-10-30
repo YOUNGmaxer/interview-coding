8a17ee8647a2757940cb116a536680e64b5bd379
azeryang <517538470@qq.com>
2022-10-30 21:06:14 +0800

    feat(leetcode): 53

--- src/algorithm/53-max-sub-array.js
+++ src/algorithm/53-max-sub-array.js
@@ -18,4 +18,18 @@ var maxSubArray = function(nums) {
   return max;
 };
 
+var maxSubArray_2 = function(nums) {
+  const len = nums.length
+  const dp = new Array(len).fill(0)
+  dp[0] = nums[0]
+  let result = dp[0]
+
+  for (let i = 1; i < len; i++) {
+      dp[i] = Math.max(dp[i-1] + nums[i], nums[i])
+      result = Math.max(result, dp[i])
+  }
+
+  return result
+};
+
 console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
