7e35fc3d3cc5936e111d531205439de47b93361c
azeryang <517538470@qq.com>
2022-11-06 16:12:13 +0800

    feat(leetcode): three-sum

--- src/algorithm/15-three-sum.js
+++ src/algorithm/15-three-sum.js
@@ -39,4 +39,43 @@ var threeSum = function(nums) {
   return res;
 };
 
+function threeSum_2(nums) {
+  const len = nums.length
+  const result = []
+
+  nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < len; i++) {
+    const target = nums[i]
+    if (target > 0) break;
+    if (i > 0 && target === nums[i-1]) continue
+
+    let left = i + 1, right = len - 1
+    while (left < right) {
+      if (left > i + 1 && nums[left] === nums[left - 1]) {
+        left++
+        continue
+      }
+      if (right < len - 1 && nums[right] === nums[right + 1]) {
+        right--
+        continue
+      }
+
+      const sum = nums[left] + nums[right] + target
+      if (sum === 0) {
+        result.push([target, nums[left], nums[right]])
+        left++
+        right--
+      } else if (sum < 0) {
+        left++
+      } else {
+        right--
+      }
+    }
+  }
+
+  return result
+}
+
 console.log(threeSum([-1,0,1,2,-1,-4]));
+console.log(threeSum_2([-1,0,1,2,-1,-4]));
