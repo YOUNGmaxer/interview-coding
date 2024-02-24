3c6db9bda0b8d44d02c320bfa3ea5bd559dbbf2e
azeryang <517538470@qq.com>
2024-02-24 17:09:52 +0800

    feat: 更新new实现

--- src/implement/new.js
+++ src/implement/new.js
@@ -1,6 +1,10 @@
-function myNew(target, ...args) {
-  const obj = Object.create(null)
-  Object.setPrototypeOf(obj, target.prototype)
-  const result = target.call(obj, ...args)
-  return typeof result === 'object' && result !== null ? result : obj
+function myNew(constructor, ...args) {
+  // 步骤1: 创建一个空的对象，对象的原型为构造函数的 prototype 属性
+  const obj = Object.create(constructor.prototype);
+  
+  // 步骤2和3: 将构造函数的上下文(this)绑定到新创建的对象上
+  const result = constructor.apply(obj, args);
+  
+  // 步骤4: 如果构造函数返回了一个对象，则返回该对象，否则返回创建的新对象
+  return result instanceof Object ? result : obj;
 }
